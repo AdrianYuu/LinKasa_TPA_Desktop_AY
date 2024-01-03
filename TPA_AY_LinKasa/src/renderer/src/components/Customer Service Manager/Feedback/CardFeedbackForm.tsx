38b41cbffb9e38a2b8596a6c19e96aa8bb35dd1a
Adrian Yu <adrianyuu01@gmail.com>
2024-01-03 23:41:01 +0700

    first commit

---
+++ TPA_AY_LinKasa/src/renderer/src/components/Customer Service Manager/Feedback/CardFeedbackForm.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const CardBroadcast = ({ id, title, question, answerType }) => {
+  return (
+    <div>
+      {id}
+      {title}
+    </div>
+  )
+}
+
+export default CardBroadcast
